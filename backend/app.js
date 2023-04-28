eb750af2e895601586c1a9afb5dafa531a54d23b
Olesia Erastova <olesia1205@yandex.ru>
2023-04-28 19:05:30 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const SERVER_ERROR = http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001', 'http://api.mesto.olerastova.nomoredomains.monster/signup'] }));
+app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001', 'http://api.mesto.olerastova.nomoredomains.monster/sign-up', 'http://api.mesto.olerastova.nomoredomains.monster'] }));
 // app.use(cors());
 app.options('*', cors());
 app.use(requestLogger);
